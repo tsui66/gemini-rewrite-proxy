c93f971c8e1adaf2917931d62f9e7332cf2e495f
tsui66 <quinton.xu@gmail.com>
2023-05-19 14:41:38 +0800

    fix: Add CORS headers to API proxy endpoint.
    - Add functionality to handle CORS headers and OPTIONS requests in `pages/api/proxy.ts`
    - Refactor `handleRequest` function to add headers and forward requests to target endpoint in `pages/api/proxy.ts`
    - Remove `src/handle-request.ts` file.

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -13,4 +13,52 @@ export const config = {
   ],
 };
 
-export default handleRequest;
+const pickHeaders = (headers: Headers, keys: (string | RegExp)[]): Headers => {
+  const picked = new Headers();
+  for (const key of headers.keys()) {
+    if (keys.some((k) => (typeof k === "string" ? k === key : k.test(key)))) {
+      const value = headers.get(key);
+      if (typeof value === "string") {
+        picked.set(key, value);
+      }
+    }
+  }
+  return picked;
+};
+
+const CORS_HEADERS: Record<string, string> = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "Content-Type, Authorization",
+};
+
+export default async function handleRequest(req: Request & { nextUrl?: URL }) {
+  if (req.method === "OPTIONS") {
+    return new Response(null, {
+      headers: CORS_HEADERS,
+    });
+  }
+
+  const { pathname, search } = req.nextUrl ? req.nextUrl : new URL(req.url);
+  const url = new URL(pathname + search, "https://generativelanguage.googleapis.com").href;
+  console.log('Debug console url:', url)
+  const headers = pickHeaders(req.headers, ["content-type", "authorization"]);
+
+  const res = await fetch(url, {
+    body: req.body,
+    method: req.method,
+    headers,
+  });
+
+  const resHeaders = {
+    ...CORS_HEADERS,
+    ...Object.fromEntries(
+      pickHeaders(res.headers, ["content-type", /^x-ratelimit-/])
+    ),
+  };
+
+  return new Response(res.body, {
+    headers: resHeaders,
+    status: res.status
+  });
+}
